bd4af2e17639d1a675309906f64e39b76c1e96b2
gillesh <gillesh@9ce8b743-0909-46e4-8230-39c73aa7ff53>
2010-03-18 14:18:36 +0000

    * Désactivation du plugin de message d'absense pour les clients
    git-svn-id: http://open.iabsis.com/subversion/igestis-roundcube/trunk@177 9ce8b743-0909-46e4-8230-39c73aa7ff53

--- usr/share/igestis/plugins/roundcube_vacation.php
+++ usr/share/igestis/plugins/roundcube_vacation.php
@@ -4,28 +4,30 @@
 if(!defined("INDEX_LAUNCHED")&& !defined("GENERAL_INDEX_REQUEST_LAUNCHED")) die("Hacking attempt");
 define(IABSIS_NEWS_URL, "http://www.iabsis.com/index.php?mact=CGFeedMaker,cntnt01,default,0&cntnt01feed=News2&cntnt01showtemplate=false&cntnt01returnid=15");
 
+if($application->userprefs['user_type'] == "employee") {
+    // Création du contenu de l'applet uniquement pour les employés
 
-// Ajout d'une occurrence de l'applet dans le getionnaire d'applet
-$applet_id = $this->add_applet("ROUNDCUBE_VACATION", "Mon message d'absence", TRUE, false);
+    // Ajout d'une occurrence de l'applet dans le getionnaire d'applet
+    $applet_id = $this->add_applet("ROUNDCUBE_VACATION", "Mon message d'absence", TRUE, false);
 
+    $data = "";
+    $file = $application->get_html_content("plugins/roundcube_vacation.htm");
+    $data = $file;
 
-// Création du contenu de l'applet
-$data = "";
-$file = $application->get_html_content("plugins/roundcube_vacation.htm");
-$data = $file;
+    $BASE_FOLDER = mount_user_folder("main") . "/";
 
-$BASE_FOLDER = mount_user_folder("main") . "/";
+    $content = "";
+    if(is_file($BASE_FOLDER . "/.procmail-vacation")) {
+            $content = file_get_contents($BASE_FOLDER . "/.procmail-vacation");
+    }
+    if(trim($content)) $application->add_var("vacation_message_activated", true);
+    umount_user_folder($application->userprefs['login']);
 
-$content = "";
-if(is_file($BASE_FOLDER . "/.procmail-vacation")) {
-	$content = file_get_contents($BASE_FOLDER . "/.procmail-vacation");
+    $data = $application->show_content($data, true);
+    // Ajout du contenu de l'applet
+    $this->set_applet_data($applet_id, $data);
 }
-if(trim($content)) $application->add_var("vacation_message_activated", true);
-umount_user_folder($application->userprefs['login']);
 
-$data = $application->show_content($data, true);
-// Ajout du contenu de l'applet
-$this->set_applet_data($applet_id, $data);
 
 
 ?>
