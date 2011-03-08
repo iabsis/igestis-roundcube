1e46bc0ffe7ed9dc47218ee7b79b3355370ce80d
gillesh <gillesh@9ce8b743-0909-46e4-8230-39c73aa7ff53>
2011-03-08 09:22:39 +0000

    * Anomalie #114 : Problème de montage samba après mise à jour vers Debian Squeeze (suppression de l'applet roundube pour le compte root)
    git-svn-id: http://open.iabsis.com/subversion/igestis-roundcube/trunk@702 9ce8b743-0909-46e4-8230-39c73aa7ff53

--- usr/share/igestis/plugins/roundcube_vacation.php
+++ usr/share/igestis/plugins/roundcube_vacation.php
@@ -3,7 +3,7 @@
 // If a little malicious guy attempt to launche this file directly, application stop with the message below ...
 if(!defined("INDEX_LAUNCHED")&& !defined("GENERAL_INDEX_REQUEST_LAUNCHED")) die("Hacking attempt");
 
-if($application->userprefs['user_type'] == "employee") {
+if($application->userprefs['user_type'] == "employee" && $application->userprefs['login'] != "root") {
     // Création du contenu de l'applet uniquement pour les employés
 
     // Ajout d'une occurrence de l'applet dans le getionnaire d'applet
@@ -16,7 +16,7 @@ if($application->userprefs['user_type'] == "employee") {
     $BASE_FOLDER = create_smb_url();
 
     $content = "";
-    $content = file_get_contents($BASE_FOLDER . "/.procmail-vacation");
+    $content = @file_get_contents($BASE_FOLDER . "/.procmail-vacation");
 
     if(trim($content)) $application->add_var("vacation_message_activated", true);
 
